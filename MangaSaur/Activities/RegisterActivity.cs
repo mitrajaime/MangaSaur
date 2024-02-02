c0e094009bea4bfbe39bb44ae1e625e895975ea7
JAIME ACT <mitrajaime@gmail.com>
2024-02-02 16:40:37 +0800

    added auth

--- MangaSaur/Activities/RegisterActivity.cs
+++ MangaSaur/Activities/RegisterActivity.cs
@@ -1,35 +1,39 @@
 ﻿using Android.App;
 using Android.Content;
+using Android.Graphics;
 using Android.OS;
-using Android.Runtime;
-using Android.Views;
 using Android.Widget;
-using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using Firebase.Firestore;
+using Android.Gms;
 using Firebase;
+using Firebase.Auth;
+using Firebase.Firestore;
 using Java.Util;
+using MangaSaur.EventListeners;
+using MangaSaur.Helpers;
+using System;
 
 namespace MangaSaur.Activities
 {
     [Activity(Label = "RegisterActivity", Theme = "@style/AppTheme")]
-   
+
     public class RegisterActivity : Activity
     {
-        TextView btnRedirect;
+        TextView btnRedirect, textViewRegistration;
         Button btnRegister;
         EditText editTextUsername, editTextPhone, editTextPassword, editTextEmail, editTextConfirmPassword;
         FirebaseFirestore db;
+        FirebaseAuth auth;
+        AppDataHelper dataHelper = new AppDataHelper();
+        TaskCompletionListeners taskCompletionListeners = new TaskCompletionListeners();
         protected override void OnCreate(Bundle savedInstanceState)
         {
             base.OnCreate(savedInstanceState);
             Xamarin.Essentials.Platform.Init(this, savedInstanceState);
             // Set our view from the "main" layout resource
             SetContentView(Resource.Layout.register_layout);
-            db = getFireStore();
+            
 
+            textViewRegistration = FindViewById<TextView>(Resource.Id.textViewRegistration);
             btnRegister = FindViewById<Button>(Resource.Id.btnRegister);
             btnRedirect = FindViewById<TextView>(Resource.Id.btnRedirect);
             editTextConfirmPassword = FindViewById<EditText>(Resource.Id.editTextConfirmPassword);
@@ -41,6 +45,10 @@ namespace MangaSaur.Activities
             btnRedirect.Click += BtnRedirect_Click;
             btnRegister.Click += BtnRegister_Click;
 
+            Typeface tf = Typeface.CreateFromAsset(Assets, "BADABOOM.TTF");
+            textViewRegistration.SetTypeface(tf, TypefaceStyle.Normal);
+            db = dataHelper.getFirestore();
+            auth = dataHelper.getFireBaseAuth();
         }
 
         private void BtnRegister_Click(object sender, EventArgs e)
@@ -49,31 +57,49 @@ namespace MangaSaur.Activities
             if (string.IsNullOrEmpty(editTextPhone.Text) || string.IsNullOrEmpty(editTextUsername.Text) || string.IsNullOrEmpty(editTextEmail.Text) ||
             string.IsNullOrEmpty(editTextPassword.Text) || string.IsNullOrEmpty(editTextConfirmPassword.Text))
             {
-                Toast.MakeText(this, "Please complete the form.", ToastLength.Short).Show();
+                Toast.MakeText(ApplicationContext, "Please complete the form.", ToastLength.Short).Show();
                 return;
             }
-            if(editTextPassword.Text.Length < 8 || editTextPassword.Text.Length > 16)
+            if (editTextPassword.Text.Length < 8 || editTextPassword.Text.Length > 16)
             {
-                Toast.MakeText(this, "Password should have 8 characters but not more than 16. Please try again", ToastLength.Short).Show();
+                Toast.MakeText(ApplicationContext, "Password should have 8 characters but not more than 16. Please try again", ToastLength.Short).Show();
                 return;
             }
-            if(editTextPassword. Text != editTextConfirmPassword. Text)
+            if (editTextPassword.Text != editTextConfirmPassword.Text)
             {
-                Toast.MakeText(this, "Password does not match. Please try again.", ToastLength.Short).Show();
+                Toast.MakeText(ApplicationContext, "Password does not match. Please try again.", ToastLength.Short).Show();
+                return;
+            }
+            if (!editTextEmail.Text.Contains("@"))
+            {
+                Toast.MakeText(ApplicationContext, "Please enter a valid email address.", ToastLength.Short).Show();
                 return;
             }
 
 
-            HashMap userMap = new HashMap();
-            userMap.Put("Username", editTextUsername.Text);
-            userMap.Put("Phone", editTextPhone.Text);
-            userMap.Put("Email", editTextEmail.Text);
+            auth.CreateUserWithEmailAndPassword(editTextEmail.Text, editTextPassword.Text)
+                .AddOnSuccessListener(this, taskCompletionListeners)
+                .AddOnFailureListener(this, taskCompletionListeners);
 
-            DocumentReference userRef = db.Collection("users").Document();
-            userRef.Set(userMap);
+            taskCompletionListeners.Success += (success, args) =>
+            {
+                HashMap userMap = new HashMap();
+                userMap.Put("Username", editTextUsername.Text);
+                userMap.Put("Phone", editTextPhone.Text);
+                userMap.Put("Email", editTextEmail.Text);
 
-            Toast.MakeText(this, "Registration successful.", ToastLength.Short).Show();
-            StartActivity(typeof(LoginActivity));
+                DocumentReference userRef = db.Collection("users").Document(auth.CurrentUser.Uid);
+                userRef.Set(userMap);
+
+                Toast.MakeText(this, "Registration successful.", ToastLength.Short).Show();
+                StartActivity(typeof(LoginActivity));
+            };
+
+            taskCompletionListeners.Failure += (failure, args) =>
+            {
+                Toast.MakeText(this, "Registration failed:" + args.Cause, ToastLength.Short).Show();
+            };
+            
         }
 
         private void BtnRedirect_Click(object sender, EventArgs e)
@@ -82,32 +108,6 @@ namespace MangaSaur.Activities
             StartActivity(lg);
         }
 
-        public FirebaseFirestore getFireStore()
-        {
-            var app = FirebaseApp.InitializeApp(this);
-
-            FirebaseFirestore database;
-
-            if(app == null)
-            {
-                var options = new FirebaseOptions.Builder()
-                    .SetProjectId("mangasaur-ee166")
-                    .SetApplicationId("mangasaur-ee166")
-                    .SetApiKey("AIzaSyCeENKvRWLecjg9z-xaypzVR2JBchQiRoE")
-                    .SetStorageBucket("mangasaur-ee166.appspot.com")
-                    .Build();
-
-                app = FirebaseApp.InitializeApp(this, options);
-                database = FirebaseFirestore.GetInstance(app);
-
-            }
-            else
-            {
-                database = FirebaseFirestore.GetInstance(app);
-            }
-
-            return database;
-        }
 
     }
 }
---
+++ MangaSaur/Helpers/AppDataHelper.cs
@@ -0,0 +1,67 @@
+﻿using Android.App;
+using Android.Content;
+using Android.OS;
+using Android.Runtime;
+using Android.Views;
+using Android.Widget;
+using Firebase.Auth;
+using Firebase.Firestore;
+using Firebase;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace MangaSaur.Helpers
+{
+    public class AppDataHelper
+    {
+        public FirebaseFirestore getFirestore()
+        {
+            var app = FirebaseApp.InitializeApp(Application.Context);
+            FirebaseFirestore database;
+
+            if (app == null)
+            {
+                var options = new FirebaseOptions.Builder()
+                     .SetProjectId("mangasaur-ee166")
+                     .SetApplicationId("mangasaur-ee166")
+                     .SetApiKey("AIzaSyCeENKvRWLecjg9z-xaypzVR2JBchQiRoE")
+                     .SetStorageBucket("mangasaur-ee166.appspot.com")
+                     .Build();
+
+                app = FirebaseApp.InitializeApp(Application.Context, options);
+                database = FirebaseFirestore.GetInstance(app);
+            }
+            else
+            {
+                database = FirebaseFirestore.GetInstance(app);
+            }
+            return database;
+        }
+
+        public FirebaseAuth getFireBaseAuth()
+        {
+            var app = FirebaseApp.InitializeApp(Application.Context);
+            FirebaseAuth mAuth;
+
+            if (app == null)
+            {
+                var options = new FirebaseOptions.Builder()
+                    .SetProjectId("mangasaur-ee166")
+                    .SetApplicationId("mangasaur-ee166")
+                    .SetApiKey("AIzaSyCeENKvRWLecjg9z-xaypzVR2JBchQiRoE")
+                    .SetStorageBucket("mangasaur-ee166.appspot.com")
+                    .Build();
+                
+                app = FirebaseApp.InitializeApp(Application.Context, options);
+                mAuth = FirebaseAuth.GetInstance(app);
+            }
+            else
+            {
+                mAuth = FirebaseAuth.GetInstance(app);
+            }
+            return mAuth;
+        }
+    }
+}
