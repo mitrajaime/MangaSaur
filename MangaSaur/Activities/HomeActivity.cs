5f208b3a198af18d1dea5ecf44a36bf22c21c19e
JAIME-ACT <ACT-STUDENT@LAB6-PC30>
2024-02-16 16:35:33 +0800

    SIGN IN BUT NOT WORKING YET

---
+++ MangaSaur/Activities/HomeActivity.cs
@@ -0,0 +1,31 @@
+﻿using Android.App;
+using Android.Content;
+using Android.Graphics;
+using Android.OS;
+using Android.Runtime;
+using Android.Views;
+using Android.Widget;
+using Firebase.Auth;
+using MangaSaur.EventListeners;
+using MangaSaur.Helpers;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace MangaSaur.Activities
+{
+        [Activity(Theme = "@style/AppTheme")]
+        public class HomeActivity : Activity
+        {
+            protected override void OnCreate(Bundle savedInstanceState)
+            {
+                base.OnCreate(savedInstanceState);
+                Xamarin.Essentials.Platform.Init(this, savedInstanceState);
+                // Set our view from the "main" layout resource
+                SetContentView(Resource.Layout.activity_main);
+
+            }
+
+        }
+    }
--- MangaSaur/Activities/LoginActivity.cs
+++ MangaSaur/Activities/LoginActivity.cs
@@ -1,10 +1,14 @@
 ﻿using Android.App;
 using Android.Content;
+using Android.Gms.Common.Data;
 using Android.Graphics;
 using Android.OS;
 using Android.Runtime;
 using Android.Views;
 using Android.Widget;
+using Firebase.Auth;
+using MangaSaur.EventListeners;
+using MangaSaur.Helpers;
 using System;
 using System.Collections.Generic;
 using System.Linq;
@@ -16,6 +20,11 @@ namespace MangaSaur.Activities
     public class LoginActivity : Activity
     {
         TextView btnRedirect, textViewMangaSaur;
+        Button btnLogin;
+        EditText editTextEmail, editTextPassword;
+        FirebaseAuth auth;
+        AppDataHelper dataHelper = new AppDataHelper();
+        TaskCompletionListeners taskCompletionListeners = new TaskCompletionListeners();
         protected override void OnCreate(Bundle savedInstanceState)
         {
             base.OnCreate(savedInstanceState);
@@ -23,14 +32,38 @@ namespace MangaSaur.Activities
             // Set our view from the "main" layout resource
             SetContentView(Resource.Layout.login_layout);
 
+            btnLogin = FindViewById<Button>(Resource.Id.btnLogin);
             btnRedirect = FindViewById<TextView>(Resource.Id.btnRedirect);
             btnRedirect.Click += BtnRedirect_Click;
+            btnLogin.Click += BtnLogin_Click;
 
             textViewMangaSaur = FindViewById<TextView>(Resource.Id.textViewMangaSaur);
 
             Typeface tf = Typeface.CreateFromAsset(Assets,"BADABOOM.TTF");
             textViewMangaSaur.SetTypeface(tf, TypefaceStyle.Normal);
+            auth = dataHelper.getFireBaseAuth();
+        }
 
+        private void BtnLogin_Click(object sender, EventArgs e)
+        {
+            //if (string.IsNullOrEmpty(editTextEmail.Text) || string.IsNullOrEmpty(editTextPassword.Text))
+            //{ 
+            //    Toast.MakeText(this, "Email or password is invalid. Please try again", ToastLength.Short).Show();
+            //    return;
+            //}
+            auth.SignInWithEmailAndPassword(editTextEmail.Text, editTextPassword.Text)
+                .AddOnSuccessListener(taskCompletionListeners)
+                .AddOnFailureListener(taskCompletionListeners);
+            taskCompletionListeners.Success += (success, args) =>
+            {
+                Toast.MakeText(this, "Login successful.", ToastLength.Short).Show();
+                StartActivity(typeof(HomeActivity));
+            };
+            taskCompletionListeners.Failure += (failure, args) =>
+            {
+                Toast.MakeText(this, "Login failed: " + args.Cause, ToastLength.Short).Show();
+                return;
+            };
         }
 
         private void BtnRedirect_Click(object sender, EventArgs e)
