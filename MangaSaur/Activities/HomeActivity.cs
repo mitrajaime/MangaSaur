a3b2eabd2b61d92d1e3bcfe96bd39d20b1809a3c
mitrajaime <mitrajaime@gmail.com>
2024-03-06 23:08:53 +0800

    changed colors sa home page menu

--- MangaSaur/Activities/HomeActivity.cs
+++ MangaSaur/Activities/HomeActivity.cs
@@ -18,6 +18,8 @@ namespace MangaSaur.Activities
         [Activity(Theme = "@style/AppTheme")]
         public class HomeActivity : Activity
         {
+            // toolbar = FindViewById<Toolbar>(Resource.Id.toolbar);
+
             protected override void OnCreate(Bundle savedInstanceState)
             {
                 base.OnCreate(savedInstanceState);
@@ -28,4 +30,40 @@ namespace MangaSaur.Activities
             }
 
         }
-    }
+        //public override bool OnOptionsItemSelected(IMenuItem item)
+        //{
+        //    int id = item.ItemId;
+
+        //    if (id == Resource.Id.action_logout)
+        //    {
+        //        AlertDialog.Builder builder = new AlertDialog.Builder(this);
+        //        builder.SetTitle("Confirm");
+        //        builder.SetMessage("Are you sure you want to log out?");
+
+        //        builder.SetPositiveButton("Confirm", (sender, e) =>
+        //        {
+        //            auth.SignOut();
+        //            Toast.MakeText(this, "Logout successful", ToastLength.Short).Show();
+        //            StartActivity(typeof(LoginActivity));
+        //        });
+
+        //        builder.SetNegativeButton("Cancel", (sender, e) => 
+        //        {
+        //            return;
+        //        });
+
+        //        Dialog dialog = builder.Create();
+        //        dialog.Show();
+        //    }
+
+        //    if(id == Resource.Id.action_refresh)
+        //    {
+        //        Toast.MakeText(this, "Refresh", ToastLength.Short).Show();
+        //    }
+        //}
+        //public override bool OnCreateOptionsMenu(IMenu menu)
+        //{
+        //    MenuInflater.Inflate(Resource.Menu.feed_menu, menu);
+        //    return true;
+        //}
+}
